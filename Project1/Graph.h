e5607305302cb7e8501047283955f3702cc79333
wangrui <775778592@qq.com>
2019-08-07 18:31:08 +0800

    单源最短路径

--- Project1/Graph.h
+++ Project1/Graph.h
@@ -2,6 +2,19 @@
 #include "stdfx.h"
 
 namespace Graph {
+	//the definity fo Graph
+	typedef vector<list<int>> Graph;
+	typedef vector<vector<int>> Graph_V;
+	typedef vector<list<pair<int, int>>> Graph_S;
+	
+	//Edge
+	struct Edge {
+		int u;
+		int v;
+		int weight;
+		Edge(int u_,int v_,int weight_):u(u_),v(v_),weight(weight_) {
+		}
+	};
 	
 
 }
